8ab6a9235e14842322542c9d828fafde6431d6c8
citrus77 <jason.lammers@outlook.com>
2022-01-11 23:11:59 -0800

    built and styled Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-    return <>
-    </>
+    return <div id='header'>
+        <div  className='title'>
+            <h1>VIEW FROM MARS</h1>
+        </div>
+        <div className='subtitle'>built using NASA's Mars Rover Photos API</div>
+    </div>
 };
 
 export default Header;
