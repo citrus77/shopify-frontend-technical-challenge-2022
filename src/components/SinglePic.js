3cbc71549fca0f9566b2d54bac2e351cad10a4b6
citrus77 <jason.lammers@outlook.com>
2022-01-16 00:40:36 -0800

    fixed localStorage bug

--- src/components/SinglePic.js
+++ src/components/SinglePic.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './css/SinglePic.css';
 
-const SinglePic = ({pic}) => {
+const SinglePic = ({ pic }) => {
     const [ fav, setfav ] = useState('fav');    
     const {
         camera,
@@ -13,47 +13,49 @@ const SinglePic = ({pic}) => {
     const { name } = rover;
     const { full_name } = camera;
 
-    const localLike = {
-        picId: pic.id,
-        classname: fav
-    }
-    const JSON_localLike = JSON.stringify(localLike);
-    const rtvdLocalLike = (localStorage.getItem(`likeStatus${pic.id}`))
-    
+    const localLike = (localStorage.getItem('pic' + id));
+
+    const handleSetLike = () => {
+        if (fav === 'fav') {
+            setfav('fav liked');
+            localStorage.setItem('pic' + id, id)
+        } else {
+            setfav('fav');
+            localStorage.removeItem('pic' + id)
+        };
+    };
+
     useEffect( () => {
         try {
-            if (rtvdLocalLike) {
-                setfav(rtvdLocalLike.classname)
+            if (localLike) {
+                setfav('fav liked')
             }
         } catch (error) {
             console.error (error);
         };
     }, [] );
 
-    return <>
-        <div className='single-pic'>
-            <img src={ img_src } alt={ pic.id } className='rover-img' />
+    return (
+        pic
+        ? <div className='single-pic'>
+                <img src={ img_src } alt={ pic.id } className='rover-img' />
             <div className='img-content'>
                 <h3 className='rover-info'>{ name } rover - { full_name }</h3>
                 <div className='date'>{ earth_date }</div>
             </div>
             <button 
                 className= { fav }
-                onClick = { () => {
-                    fav === 'fav'
-                        ? setfav('fav liked')
-                        : setfav('fav')
-                    localStorage.setItem(`likeStatus${pic.id}`, JSON_localLike)
-                }}
+                onClick = { handleSetLike }
             >
             {
-            fav === 'fav'
-                ? <img src='./img/heart.png' alt='unliked' height='32' width='32'/>
-                : <img src='./img/heartfilled.png' alt='liked' height='32' width='32'/>
+                fav === 'fav'
+                    ? <img src='./img/heart.png' alt='unliked' height='32' width='32'/>
+                    : <img src='./img/heartfilled.png' alt='liked' height='32' width='32'/>
             }
             </button>
         </div>
-    </>;
+        : '...Loading...'
+    );
 };
 
 export default SinglePic;
